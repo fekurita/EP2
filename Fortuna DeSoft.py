41ce8cd9a771527c16c081172b19261d5d6f28b4
fekurita <felipekurita@gmail.com>
2022-11-06 14:23:29 -0300

    Introdução

---
+++ Fortuna DeSoft.py
@@ -0,0 +1,3 @@
+print ("Olá! Você está na Fortuna DeSoft e terá a oportunidade de enriquecer!")
+Nome =input("Qual seu nome?")
+print("Ok {0}, você tem direito a pular 3 vezes e 2 ajudas!"'\n'"As opções de resposta são A, B, C, D, ajuda, pula e parar!".format(Nome.upper()))
