632b51df2f5e4d46e34c48df585e2a41ebd79a62
fekurita <felipekurita@gmail.com>
2022-11-06 16:07:08 -0300

    Jogo basico

--- Fortuna DeSoft.py
+++ Fortuna DeSoft.py
@@ -4,6 +4,9 @@ from Questoes import *
 Questoespn =transforma_base(quest)
 Questoesusa =[]
 
+id = 1
+Lpremio=[1000,5000,10000,30000,50000,100000,300000,500000,1000000]
+
 print ("Olá! Você está na Fortuna DeSoft e terá a oportunidade de enriquecer!"'\n')
 Nome =input("Qual seu nome?")
 print('\n'"Ok {0}, você tem direito a pular 3 vezes e 2 ajudas!"'\n'"As opções de resposta são A, B, C, D, ajuda, pula e parar!"'\n'.format(Nome.upper()))
@@ -11,6 +14,8 @@ Comeco =input("Aperte ENTER para continuar...")
 
 T = True
 T2 = True
+T3 = True
+T4 = True
 while T == True:
     if Comeco == '':
         T = False
@@ -19,8 +24,29 @@ while T == True:
         while T2 == True:
             if Continuar =="":
                 T2 = False
-                print('\n''\n')
-                print(sorteia_questao_inedida(Questoespn,'facil',Questoesusa))
+                while T3 == True and id<10:
+                    if id<3:
+                        nivel = "facil"
+                    elif id>3 and id<7:
+                        nivel = "medio"
+                    else:
+                        nivel = "dificil"
+                    print('\n''\n')
+                    questaoatual = sorteia_questao_inedida(Questoespn,nivel,Questoesusa)
+                    print(questao_para_texto(questaoatual,id))
+                    resposta = input('\n'"Qual sua resposta?!")
+                    if resposta == questaoatual["correta"]:
+                        print ("Você acertou! Seu prêmio atula é de R$ {}".format(Lpremio[id-1]))
+                        id +=1
+                        while T4 == True:
+                            Continuarfinal =input("Aperte ENTER para continuar...")
+                            if Continuarfinal == "":
+                                T4 = False
+                            else:
+                                Continuarfinal =input("Aperte ENTER para continuar...")
+                    else: 
+                        print ("Que pena! Você errou e vai sair sem nada :(") 
+                        T3 = False
 
 
             else:
